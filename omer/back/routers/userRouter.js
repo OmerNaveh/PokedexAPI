f81e937e53cd985c6025347959ea6893aa137949
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2021-10-23 18:32:35 +0300

    Update userRouter.js

--- omer/back/routers/userRouter.js
+++ omer/back/routers/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 router.use(express.json())
 
 router.post('/', (request, response) => {
-    const user = request.body.username
+    const user = request.headers.username
     return response.send(JSON.stringify({ "usermane": user }))
 })
 
